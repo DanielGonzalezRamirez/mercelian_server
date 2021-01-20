a3340f01d0663f5beca0d92e9c3d8e9363633e77
DanielGonzalezRamirez <danielj.gonzalezr@gmail.com>
2021-01-20 12:25:57 -0500

    Add files via upload

---
+++ bash/initial_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update and upgrade machine
+apt update
+apt upgrade
+
+# Install nginx server, certbot for SSL and rsync for loading files
+apt install nginx
+apt install python3-certbot-nginx
+apt install rsync
